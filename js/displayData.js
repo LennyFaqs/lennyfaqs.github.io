b935092307f05f0c159cfcf7ef82c06e4a8b621f
Ryuggo <hugofrancois12@gmail.com>
2022-03-21 09:54:21 +0100

    display value test

--- js/displayData.js
+++ js/displayData.js
@@ -2,10 +2,13 @@ function displayData(obj)
 {
     var display = document.getElementsByClassName("display");
     console.log(obj);
-    console.log(obj.value);
-    console.log(obj.firstChild);
     console.log(obj.firstChild.nodeValue);
-    const txt = obj.firstChild.nodeValue.replace(/\s/g, '').charAt(0).toLowerCase();
+
+    let txt = obj.firstChild.nodeValue.replace(/\s/g, '');
     console.log(txt);
+    txt = txt.charAt(0).toLowerCase();
+    console.log(txt);
+
+    console.log(display);
     display.setAttribute("data-include", txt);
 };
