87d950810081b7486a6e5b89d0a334462662adcb
Ryuggo <hugofrancois12@gmail.com>
2022-03-21 09:31:16 +0100

    fix 2

--- js/displayData.js
+++ js/displayData.js
@@ -1,8 +1,7 @@
 function displayData(obj)
 {
     var display = document.getElementsByClassName("display");
-    const txt = obj.innerHtml.replace(/\s/g, '')
-                            .charAt(0).toLowerCase()
+    const txt = obj.innerHtml.replace(/\s/g, '').charAt(0).toLowerCase();
     console.log(txt);
     display.setAttribute("data-include", obj.innerHtml);
 };
