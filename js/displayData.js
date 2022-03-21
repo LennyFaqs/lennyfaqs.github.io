5a297f8e3aef6bcb5042aeb51de14b0daada71e0
Ryuggo <hugofrancois12@gmail.com>
2022-03-21 10:02:25 +0100

    fix8

--- js/displayData.js
+++ js/displayData.js
@@ -1,10 +1,11 @@
 function displayData(obj)
 {
     //var parentDOM = document.getElementById("parent-id");
+    console.log(document);
     var display = document.getElementsByClassName("displayyy")[0];
 
     let txt = obj.firstChild.nodeValue.replace(/\s/g, '');
-    let txt2 = txt.charAt(0).toLowerCase();
+    let txt2 = txt[0].toLowerCase();
     console.log(txt);
     console.log(txt2);
 
