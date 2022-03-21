82ede5ccf9ac6b5623030ff5acf60afba9c0bb7b
Ryuggo <hugofrancois12@gmail.com>
2022-03-21 10:44:55 +0100

    fix12

--- js/displayData.js
+++ js/displayData.js
@@ -4,8 +4,9 @@ function displayData(obj)
 
     let txt = obj.firstChild.nodeValue.replace(/\s/g, '');
     txt = txt[0].toLowerCase() + txt.slice(1);
+    console.log(obj.class);
 
-    display.innerHTML = '<div data-include="html/'+ txt +'.html"></div>';
+    display.innerHTML = '<div data-include="html/data/'+ obj.class +'/'+ txt +'.html"></div>';
     reloadFiles();
     //display.setAttribute("data-include", txt);
 };
