2a4df8c7edbcbaf17f340044fe759cace1c5932b
Ryuggo <hugofrancois12@gmail.com>
2022-03-24 17:09:50 +0100

    load

--- js/onLoad.js
+++ js/onLoad.js
@@ -1,6 +1,6 @@
 window.onload = async function() {
     reloadFiles();
-    await new Promise(r => setTimeout(r, 1000));
+    await new Promise(r => setTimeout(r, 500));
     reloadFiles();
 
     document.getElementsByClassName("date")[0].innerHTML = "All right Reserved © 2022 - " + new Date().getFullYear();
